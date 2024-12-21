d0cefcc6d891463feb616ac9593c399b25442f69
jahanRazh <jahanrazh@gmail.com>
2024-12-22 00:06:42 +0530

    update main.dart to disable debug banner and wrap Scaffold in SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,22 @@ class MyAPP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Container(
-              height: 100,
-              width: 100,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(5),
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(5),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
